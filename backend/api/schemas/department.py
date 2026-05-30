ad7ec7630bd168d5f44979e2ccad2654a7fe7dd4
Kirill Balaganskii <kirillbalaghanskiii@gmail.com>
2026-05-30 22:20:45 +0600

    add schema departments

---
+++ backend/api/schemas/department.py
@@ -0,0 +1,16 @@
+# build a schema using pydantic
+from typing import List, Literal, Union, Optional, Annotated, Dict, Any
+from datetime import date, datetime
+#from enum import Enum
+
+from pydantic import BaseModel, StringConstraints, Field, field_validator
+
+
+class Department(BaseModel):
+
+    id: int = Field(..., title="Индификатор подразделения", description='Индификатор подразделения')
+    name: Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Название подразделения", description='Название подразделения')
+    parent_id: Optional[int]= Field(None, title="Название родительского подразделения", description='Название родительского подразделения если есть (поле может быть Null)')
+    created_at: datetime = Field(..., title="Дата создания", description='Дата создания')
+
+

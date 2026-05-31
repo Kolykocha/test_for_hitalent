a355f8918e21c689c7ed3b2f7d9c3b578a3c8f90
Kirill Balaganskii <kirillbalaghanskiii@gmail.com>
2026-05-31 16:13:56 +0600

    return scheme add

--- backend/api/schemas/department.py
+++ backend/api/schemas/department.py
@@ -8,9 +8,11 @@ from pydantic import BaseModel, StringConstraints, Field
 
 class Department(BaseModel):
 
-    id: int = Field(..., title="Индификатор подразделения", description='Индификатор подразделения')
     name: Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Название подразделения", description='Название подразделения')
     parent_id: Optional[int]= Field(None, title="Название родительского подразделения", description='Название родительского подразделения если есть (поле может быть Null)')
     created_at: datetime = Field(..., title="Дата создания", description='Дата создания')
 
 
+class DepartmentReturn(Department):  
+    
+    id: int = Field(..., title="Индификатор подразделения", description='Индификатор подразделения')
--- backend/api/schemas/employee.py
+++ backend/api/schemas/employee.py
@@ -8,11 +8,14 @@ from pydantic import BaseModel, StringConstraints, Field
 
 class Employee(BaseModel):
 
-    id: int = Field(..., title="Индификатор сотрудника", description='Индификатор сотрудника')
     department_id: int = Field(..., title="Предприятие", description='Предприятие за которым закреплен сотрудник')
     full_name : Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Имя", description='Имя сотрудника')
     position : Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Должность", description='Должность сотрудника')
     hired_at: Optional[date]= Field(None, title="Дата трудоустройства сотрудника", description='Дата трудоустройства сотрудника')
     created_at: datetime = Field(..., title="Дата создания", description='Дата создания')
 
+
+class EmployeeReturn(Employee):  
+
+    id: int = Field(..., title="Индификатор сотрудника", description='Индификатор сотрудника')
 

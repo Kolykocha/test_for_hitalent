8761bd962a3612192453801cd3539da8d3dae14a
Kirill Balaganskii <kirillbalaghanskiii@gmail.com>
2026-05-31 16:40:13 +0600

    refactor schemas

--- backend/api/schemas/department.py
+++ backend/api/schemas/department.py
@@ -1,19 +1,34 @@
 
-from typing import  Optional, Annotated
+from typing import  Optional, Annotated, List
 from datetime import  datetime
 
+from schemas.employee import Employee
 
 from pydantic import BaseModel, StringConstraints, Field
 
 
 class Department(BaseModel):
-
+    id: int = Field(..., title="Индификатор подразделения", description='Индификатор подразделения')
+    created_at: datetime = Field(..., title="Дата создания", description='Дата создания')
     name: Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Название подразделения", description='Название подразделения')
     parent_id: Optional[int]= Field(None, title="Название родительского подразделения", description='Название родительского подразделения если есть (поле может быть Null)')
     
+class DepartmentCreate(BaseModel):
+    
+    name: Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Название подразделения", description='Название подразделения')
+    parent_id: Optional[int]= Field(None, title="Название родительского подразделения", description='Название родительского подразделения если есть (поле может быть Null)')
+        
 
 
 class DepartmentReturn(Department):  
 
     id: int = Field(..., title="Индификатор подразделения", description='Индификатор подразделения')
     created_at: datetime = Field(..., title="Дата создания", description='Дата создания')
+
+
+
+
+class DepartmentTree(BaseModel):
+    department : Department
+    employees : List[Employee] = []
+    children: List[Department] = []
--- backend/api/schemas/employee.py
+++ backend/api/schemas/employee.py
@@ -5,8 +5,13 @@ from datetime import  datetime,date
 
 from pydantic import BaseModel, StringConstraints, Field
 
-
 class Employee(BaseModel):
+
+    full_name : Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Имя", description='Имя сотрудника')
+    position : Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Должность", description='Должность сотрудника')
+    hired_at: Optional[date]= Field(None, title="Дата трудоустройства сотрудника", description='Дата трудоустройства сотрудника')
+
+class EmployeeCreate(BaseModel):
 
     full_name : Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Имя", description='Имя сотрудника')
     position : Annotated[str,StringConstraints(max_length=200)] = Field(..., title="Должность", description='Должность сотрудника')
